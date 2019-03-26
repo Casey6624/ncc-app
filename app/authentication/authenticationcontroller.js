1db9bfb47ffb3b0c5cf7ae86bb5facd0c9a83fbc
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-03-26 21:39:35 +0000

    Jacob has added the login faciliaties

--- app/authentication/authenticationcontroller.js
+++ app/authentication/authenticationcontroller.js
@@ -1,32 +1,41 @@
-﻿angular.module("nccAuthentication").controller("authenticationController", function ($scope, $http) {
+﻿angular
+  .module("nccAuthentication")
+  .controller("authenticationController", function($scope, $http) {
+    $scope.APIUrl = "http://webteach_net.hallam.shu.ac.uk/acesjas/api/login";
 
-    $scope.APIUrl = "http://webteach_net.hallam.shu.ac.uk/acesjas/api/login"
+    $scope.name,
+      $scope.role,
+      $scope.authenticated,
+      $scope.apiErrorMessage,
+      $scope.loginErrorMessage,
+      ($scope.signedInShow = false),
+      ($scope.signedOutShow = true);
 
-    $scope.name, $scope.role, $scope.authenticated , $scope.apiErrorMessage, $scope.loginErrorMessage
+    $scope.login = function() {
+      let authDetails = {
+        username: $scope.username,
+        password: $scope.password
+      };
 
-    $scope.login = function () {
-
-        let authDetails = {
-            username: $scope.username,
-            password: $scope.password,
-        }
-
-        $http.post($scope.APIUrl, authDetails)
-            .success(function (res) {
-                console.log(res)
-                let { authenticated, role } = res
-                $scope.authenticated = authenticated
-                if ($scope.authenticated == false) {
-                    $scope.loginErrorMessage = true
-                } else if ($scope.authenticated == true) {
-                    $scope.role = role
-                    $scope.loginErrorMessage = false
-                    console.log("Logged In!")
-                }
-            })
-            .error(function (error) {
-                console.log(error)
-                $scope.apiErrorMessage = error
-            })
-    }
-})
+      $http
+        .post($scope.APIUrl, authDetails)
+        .success(function(res) {
+          console.log(res);
+          let { authenticated, role } = res;
+          $scope.authenticated = authenticated;
+          if ($scope.authenticated == false) {
+            $scope.loginErrorMessage = true;
+          } else if ($scope.authenticated == true) {
+            $scope.role = role;
+            $scope.loginErrorMessage = false;
+            $scope.signedInShow = true;
+            $scope.signedOutShow = false;
+            console.log("Logged In!");
+          }
+        })
+        .error(function(error) {
+          console.log(error);
+          $scope.apiErrorMessage = error;
+        });
+    };
+  });
